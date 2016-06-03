04b4686b7138fb5d11d2247235b0c3871e6ff67d
bmoknine <moknine.b@gmail.com>
2016-06-03 11:53:48 +0200

    Commit de la structure(Partie/Joueur/Bateau)

---
+++ src/main/java/Joueur/classBateau.java
@@ -0,0 +1,23 @@
+package Joueur;
+
+
+public class classBateau {
+    
+	private int taille;
+
+	public classBateau(int taille) {
+		super();
+		this.taille = taille;
+	}
+
+	public int getTaille() {
+		return taille;
+	}
+
+	public void setTaille(int taille) {
+		this.taille = taille;
+	}
+	
+	
+	
+}
---
+++ src/main/java/Joueur/classJoueur.java
@@ -0,0 +1,75 @@
+package Joueur;
+
+
+public class classJoueur 
+{
+	private String nom;
+	private classBateau porteavion;
+	private classBateau croiseur;
+	private classBateau contretorpilleur;
+	private classBateau sousmarin;
+	private classBateau torpilleur;
+	
+	public classJoueur(String nom, classBateau porteavion, classBateau croiseur, classBateau contretorpilleur,
+			classBateau sousmarin, classBateau torpilleur) {
+		super();
+		this.nom = nom;
+		this.porteavion = porteavion;
+		this.croiseur = croiseur;
+		this.contretorpilleur = contretorpilleur;
+		this.sousmarin = sousmarin;
+		this.torpilleur = torpilleur;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public classBateau getPorteavion() {
+		return porteavion;
+	}
+
+	public void setPorteavion(classBateau porteavion) {
+		this.porteavion = porteavion;
+	}
+
+	public classBateau getCroiseur() {
+		return croiseur;
+	}
+
+	public void setCroiseur(classBateau croiseur) {
+		this.croiseur = croiseur;
+	}
+
+	public classBateau getContretorpilleur() {
+		return contretorpilleur;
+	}
+
+	public void setContretorpilleur(classBateau contretorpilleur) {
+		this.contretorpilleur = contretorpilleur;
+	}
+
+	public classBateau getSousmarin() {
+		return sousmarin;
+	}
+
+	public void setSousmarin(classBateau sousmarin) {
+		this.sousmarin = sousmarin;
+	}
+
+	public classBateau getTorpilleur() {
+		return torpilleur;
+	}
+
+	public void setTorpilleur(classBateau torpilleur) {
+		this.torpilleur = torpilleur;
+	}
+	
+	
+	
+	
+}
