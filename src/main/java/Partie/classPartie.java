32089e4e8860d80e6792bcea8b9bbd3ec86e2fbc
bmoknine <moknine.b@gmail.com>
2016-06-03 12:38:56 +0200

    structure de la partie

---
+++ src/main/java/Partie/classPartie.java
@@ -0,0 +1,14 @@
+package Partie;
+
+import java.util.ArrayList;
+
+import Joueur.classBateau;
+import Joueur.classJoueur;
+
+public class classPartie 
+{
+	private classJoueur JoueurA;
+	private classJoueur JoueurB;
+
+	
+}
