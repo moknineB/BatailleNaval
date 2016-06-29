aab1741fd1e7cf24ded78f3fd9b84e0ad2133254
benji007 <benjidavidinfo@gmail.com>
2016-06-29 12:30:07 +0200

    Modification sur le main

--- src/main/java/Partie/classPartie.java
+++ src/main/java/Partie/classPartie.java
@@ -1,13 +1,22 @@
 package Partie;
 
 import java.util.ArrayList;
+import Graphique.GraphePlateau;
 import Joueur.classBateau;
 import Joueur.classJoueur;
 
 public class classPartie 
 {
-	private classJoueur JoueurA;
-	private classJoueur JoueurB;
+	public static void main(String[] args)
+	  {
+	    classPartie game = new classPartie(10);
+	  }
+
+	  public classPartie(int size)
+	  {
+	    GraphePlateau plateau = new GraphePlateau(size);
+	  }
+
 
 	
 }
